7b8963c221aeb8c6e30235f6fa85474e238494bc
H Nikson <photographynikson@gmail.com>
2024-02-22 02:02:02 +0530

    Second Implementation
    Add a Search bar and a Gnav bar as the bottom navigation bar

--- photograohy_app/lib/pages/Home_Page.dart
+++ photograohy_app/lib/pages/Home_Page.dart
@@ -1,3 +1,5 @@
+import 'package:easy_search_bar/easy_search_bar.dart';
+
 import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 
@@ -11,19 +13,35 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  String searchValue = '';
+  final List<String> _suggestions = [
+    'Afeganistan',
+    'Albania',
+    'Algeria',
+    'Australia',
+    'Brazil',
+    'German',
+    'Madagascar',
+    'Mozambique',
+    'Portugal',
+    'Zambia'
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
+      appBar: EasySearchBar(
+        onSearch: (value) => setState(() => searchValue = value),
+        suggestions: _suggestions,
+        backgroundColor: Color.fromARGB(255, 126, 126, 126),
         actions: <Widget>[
           IconButton(
               onPressed: () {
                 print("Click Action");
               },
-              icon: const Icon(Icons.share))
+              icon: const Icon(Icons.share)),
         ],
+        title: const Text(''),
       ),
       drawer: Drawer(
         child: ListView(
@@ -43,31 +61,36 @@ class _HomeState extends State<Home> {
       ),
       body: const Column(
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Padding(
-                padding: EdgeInsets.all(20.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Hello User",
-                      style: TextStyle(
-                        fontWeight: FontWeight.w600,
-                        fontSize: 22,
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(top: 30),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Hello User",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 22,
+                        ),
                       ),
-                    ),
-                    Text("Good morning.."),
-                  ],
+                      Text("Good morning.."),
+                    ],
+                  ),
                 ),
-              ),
-              IconButton(
-                onPressed: null,
-                icon: Icon(Icons.person),
-                iconSize: 50,
-              ),
-            ],
+                IconButton(
+                  alignment: Alignment.bottomRight,
+                  padding: EdgeInsets.only(top: 30),
+                  onPressed: null,
+                  icon: Icon(Icons.person),
+                  iconSize: 50,
+                ),
+              ],
+            ),
           ),
           SingleChildScrollView(
             child: Column(
@@ -77,9 +100,17 @@ class _HomeState extends State<Home> {
         ],
       ),
       bottomNavigationBar: const GNav(
+        duration: Duration(milliseconds: 800),
+        gap: 8,
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        //tabActiveBorder:
+        //Border.all(color: Theme.of(context).colorScheme.inversePrimary),
+        //tabMargin: EdgeInsets.only(bottom: 4),
+        backgroundColor: Color.fromARGB(255, 126, 126, 126),
+        // ignore: prefer_const_literals_to_create_immutables
         tabs: [
           GButton(
-            icon: Icons.home,
+            icon: Icons.home_outlined,
             text: 'Home',
           ),
           GButton(
@@ -91,7 +122,7 @@ class _HomeState extends State<Home> {
             text: 'Search',
           ),
           GButton(
-            icon: Icons.settings,
+            icon: Icons.settings_outlined,
             text: 'Settings',
           ),
         ],
--- photograohy_app/lib/pages/home_shortcuts.dart
+++ photograohy_app/lib/pages/home_shortcuts.dart
@@ -7,80 +7,114 @@ class homeShortcuts extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          Container(
-            height: MediaQuery.of(context).size.height / 1.46,
-            decoration: BoxDecoration(
-              borderRadius: const BorderRadius.only(
-                  topRight: Radius.circular(25), topLeft: Radius.circular(25)),
-              color: Theme.of(context).colorScheme.background,
-              //border: Border.all(
-              //color: Theme.of(context).colorScheme.inversePrimary),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(30.0),
-              child: GridView.count(
-                crossAxisSpacing: 10,
-                mainAxisSpacing: 10,
-                crossAxisCount: 2,
-                children: <Widget>[
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    color: Colors.amber.shade800,
-                    child: const Text("He'd have you all unravel at the"),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    color: Colors.lightGreen.shade500,
-                    child: const Text('Heed not the rabble'),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    color: Colors.pink.shade600,
-                    child: const Text('Sound of screams but the'),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    color: Colors.teal[700],
-                    child: const Text('Who scream'),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    color: Colors.indigo.shade500,
-                    child: const Text('Revolution is coming...'),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    color: Colors.purple.shade500,
-                    child: const Text('Revolution, they...'),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    color: Colors.green.shade500,
-                    child: const Text('Sound of screams but the'),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    color: Colors.lightBlue.shade500,
-                    child: const Text('Who scream'),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    color: Colors.red.shade500,
-                    child: const Text('Revolution is coming...'),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    color: Colors.teal[600],
-                    child: const Text('Revolution, they...'),
-                  ),
-                ],
+    return Padding(
+      padding: const EdgeInsets.only(top: 10),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height / 1.46,
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.only(
+                    topRight: Radius.circular(25),
+                    topLeft: Radius.circular(25)),
+                color: Theme.of(context).colorScheme.background,
+                //border: Border.all(
+                //color: Theme.of(context).colorScheme.inversePrimary),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(30.0),
+                child: GridView.count(
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                  crossAxisCount: 2,
+                  children: <Widget>[
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        color: const Color.fromARGB(255, 126, 126, 126),
+                      ),
+                      padding: const EdgeInsets.all(8),
+                      child: const Text("He'd have you all unravel at the"),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        color: const Color.fromARGB(255, 160, 134, 134),
+                      ),
+                      padding: const EdgeInsets.all(8),
+                      child: const Text('Heed not the rabble'),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        color: Color.fromARGB(255, 137, 107, 194),
+                      ),
+                      padding: const EdgeInsets.all(8),
+                      child: const Text('Sound of screams but the'),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        color: const Color.fromARGB(255, 74, 150, 141),
+                      ),
+                      padding: const EdgeInsets.all(8),
+                      child: const Text('Who scream'),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        color: Colors.indigo.shade500,
+                      ),
+                      padding: const EdgeInsets.all(8),
+                      child: const Text('Revolution is coming...'),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        color: Colors.purple.shade500,
+                      ),
+                      padding: const EdgeInsets.all(8),
+                      child: const Text('Revolution, they...'),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        color: Colors.green.shade500,
+                      ),
+                      padding: const EdgeInsets.all(8),
+                      child: const Text('Sound of screams but the'),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        color: Colors.lightBlue.shade500,
+                      ),
+                      padding: const EdgeInsets.all(8),
+                      child: const Text('Who scream'),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        color: Colors.red.shade500,
+                      ),
+                      padding: const EdgeInsets.all(8),
+                      child: const Text('Revolution is coming...'),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        color: Colors.teal[600],
+                      ),
+                      padding: const EdgeInsets.all(8),
+                      child: const Text('Revolution, they...'),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
